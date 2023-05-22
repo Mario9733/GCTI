65d3837c90f654ff32d9011824e05be864b150fe
Mario9733 <mariogabriel9733@hotmail.com>
2023-05-22 20:23:30 -0300

    alterações para compilar e demonstrar em aula

--- app/src/main/java/com/example/gcti/ti/ITDepartmentActivity.kt
+++ app/src/main/java/com/example/gcti/ti/ITDepartmentActivity.kt
@@ -5,8 +5,33 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.gcti.R
 
 class ITDepartmentActivity : AppCompatActivity() {
+
+
+    private val validUsername = "admin"
+    private val validPassword = "password"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_it_department)
+
+//        btnLogin.setOnClickListener {
+//            val username = etUsername.text.toString()
+//            val password = etPassword.text.toString()
+//
+//            if (isValidCredentials(username, password)) {
+//                 Credenciais válidas, navegue para a MainActivity
+//                val intent = Intent(this, ITDepartment2::class.java)
+//                startActivity(intent)
+//                finish()
+//            } else {
+//                 Credenciais inválidas, exiba uma mensagem de erro
+//                tvError.text = getString(R.string.invalid_credentials)
+//            }
+//        }
     }
+//    private fun isValidCredentials(username: String, password: String): Boolean {
+//        return username == validUsername && password == validPassword
+//    }
+
+
 }
